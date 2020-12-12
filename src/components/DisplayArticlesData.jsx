6cc3a45679fe9f2e49c04c6f131565d6cf6ea2e5
Rithreaksa <wonderfulreaksa@gmail.com>
2020-12-12 10:44:26 +0100

    Modify subheader to subtitle in DisplayArticlesData component

--- src/components/DisplayArticlesData.jsx
+++ src/components/DisplayArticlesData.jsx
@@ -37,7 +37,7 @@ const DisplayArticlesData = () => {
               textAlign: "center",
             }}
           >
-            {article.subheader}
+            {article.sub_title}
           </Card.Meta>
           <Image
             data-cy="image"
