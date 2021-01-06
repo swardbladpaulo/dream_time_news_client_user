1c40615e1d35dca2b35010591cbcd3a59638776c
viktor <viktorkarlsson125@gmail.com>
2021-01-06 16:23:09 +0100

    readux setup

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import axios from 'axios'
+import axios from "axios";
+import store from "./state/store/configureStore";
+import { Provider } from "react-redux";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "production"
-  ? "https://dream-time-news-api.herokuapp.com"
-  : "http://localhost:3000/api";
-  
+    ? "https://dream-time-news-api.herokuapp.com"
+    : "http://localhost:3000/api";
+
+    window.store = store
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
 
---
+++ src/state/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+import initialState from "../store/initialState"
+
+const rootReducer = (state = initialState, action) => {
+  switch (key) {
+    case value:
+      
+      break;
+  
+    default:
+      break;
+  }
+} 
+
+export default rootReducer
