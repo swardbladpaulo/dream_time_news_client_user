c434900d903ddc7ce0d7dbc13b1525573ae3b68c
flis_ann <ann.flismark@gmail.com>
2021-01-07 20:04:48 +0100

    Adding NavLink to the back-button

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,42 +1,40 @@
-import React, { useEffect } from 'react';
-import { Button, Grid } from 'semantic-ui-react';
-import { getArticleData } from '../modules/articlesData';
-import { useDispatch, useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
+import React, { useEffect } from "react";
+import { Button, Grid } from "semantic-ui-react";
+import { getArticleData } from "../modules/articlesData";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useParams } from "react-router-dom";
 
 const SingleArticle = () => {
-  const dispatch = useDispatch()
-  const { singleArticle, errorMessage } =  useSelector((state) => state)
-  const { id } = useParams()
+  const dispatch = useDispatch();
+  const { singleArticle, errorMessage } = useSelector((state) => state);
+  const { id } = useParams();
 
   useEffect(() => {
-    getArticleData.show(id, dispatch)
-  }, [id, dispatch])
+    getArticleData.show(id, dispatch);
+  }, [id, dispatch]);
   return (
     <>
-      {singleArticle && (
-        <>
-          <Grid>
-            <Grid.Column width={9}>
-              <h1 data-cy="article-title">{singleArticle.title}</h1>
-              <h3 data-cy="article-sub-title">{singleArticle.sub_title}</h3>
-              <p data-cy="article-content">{singleArticle.content}</p>
-            </Grid.Column>
-            <Grid.Column width={4}>
-              <p data-cy="article-created-at">{`Created at: ${singleArticle.created_at}`}</p>
-              <p data-cy="article-updated-at">{`Updated at: ${singleArticle.updated_at}`}</p>
-              <p data-cy="article-author">{singleArticle.author}</p>
-            </Grid.Column>
-          </Grid>
-          <Button data-cy="back-button" onClick={() => getArticleData()}>
-            back
-          </Button>
-        </>
-      )}
+      <Grid>
+        <Grid.Column width={9}>
+          <h1 data-cy="article-title">{singleArticle.title}</h1>
+          <h3 data-cy="article-sub-title">{singleArticle.sub_title}</h3>
+          <p data-cy="article-content">{singleArticle.content}</p>
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <p data-cy="article-created-at">{`Created at: ${singleArticle.created_at}`}</p>
+          <p data-cy="article-updated-at">{`Updated at: ${singleArticle.updated_at}`}</p>
+          <p data-cy="article-author">{singleArticle.author}</p>
+        </Grid.Column>
+      </Grid>
+      {/* <Button data-cy="back-button" onClick={() => getArticleData()}>
+        back
+      </Button> */}
+      <Button data-cy="back-button" as={NavLink} to="/">
+        back
+      </Button>
     </>
   );
 };
-
 
 // const SingleArticle = ({ singleArticle, getArticles }) => {
 //   return (
