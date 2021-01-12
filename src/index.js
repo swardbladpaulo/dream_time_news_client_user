ddaecaa3ca620c1fcfa3989ed3a2c6e1c40044cc
Rithreaksa <wonderfulreaksa@gmail.com>
2021-01-12 17:20:28 +0100

    fixing production environment

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import axios from 'axios'
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "production"
-  ? "https://dream-time-news-api.herokuapp.com"
-  : "http://localhost:3000/api";
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+  
   
 ReactDOM.render(
   <App />,
