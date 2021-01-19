cfbc6fe6e478e658d4f3435b96f7ffe945bc4829
swardbladpaulo <paulo@swardblads.se>
2021-01-19 20:52:57 +0100

    moved login button

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,46 +1,56 @@
-import React from 'react'
-import { Form, Button, Icon, Message, Segment } from 'semantic-ui-react'
-import { useDispatch, useSelector } from 'react-redux'
-import { performAuthentication } from '../modules/auth'
+import React from "react";
+import { Form, Button, Icon, Message, Segment } from "semantic-ui-react";
+import { useDispatch, useSelector } from "react-redux";
+import { performAuthentication } from "../modules/auth";
 
 const LoginForm = () => {
-  const dispatch = useDispatch()
-  const errorMessage = useSelector((state) => state.errorMessage)
+  const dispatch = useDispatch();
+  const errorMessage = useSelector((state) => state.errorMessage);
   return (
     <>
-      <div>LoginForm</div>
+      <div
+        style={{
+          textAlign: "center",
+          fontWeight: "bold",
+          fontSize: 24,
+          color: "red",
+        }}
+      >
+        LoginForm
+      </div>
       <Segment placeholder>
-      <Form
-        id="LoginForm"
-        data-cy="login-form"
-        onSubmit={e => performAuthentication (e, dispatch)}>
-        
-        <Form.Input
-          data-cy="email"
-          icon="at"
-          type="text"
-          label="Email"
-          name="email"
-          placeholder="Email LOGIN"
-          iconPosition="left"
-        />
-        <Form.Input
-          data-cy="password"
-          icon="key"
-          label="Password"
-          placeholder="Password LOGIN"
-          iconPosition="left"
-        />  
-        <Button data-cy="login-btn" icon labelPosition="left">
-          <Icon name="user"></Icon>
-          Login
-        </Button>
-        {errorMessage &&
-          <Message data-cy="error-message">{errorMessage}</Message>}
+        <Form
+          id="LoginForm"
+          data-cy="login-form"
+          onSubmit={(e) => performAuthentication(e, dispatch)}
+        >
+          <Form.Input
+            data-cy="email"
+            icon="at"
+            type="text"
+            label="Email"
+            name="email"
+            placeholder="Enter your Email"
+            iconPosition="left"
+          />
+          <Form.Input
+            data-cy="password"
+            icon="key"
+            label="Password"
+            placeholder="Enter your Password"
+            iconPosition="left"
+          />
+          <Button data-cy="login-btn" icon labelPosition="left" color="red">
+            <Icon name="user"></Icon>
+            Login
+          </Button>
+          {errorMessage && (
+            <Message data-cy="error-message">{errorMessage}</Message>
+          )}
         </Form>
-    </Segment>
+      </Segment>
     </>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
--- src/components/MenuHeader.jsx
+++ src/components/MenuHeader.jsx
@@ -1,9 +1,9 @@
-import React, { useState }from "react";
+import React, { useState } from "react";
 import { Menu, Segment, Image, Button, Icon, Label } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import MasterModal from "./MasterModal";
 import i18n from "../i18n";
-import LoginForm from './LoginForm'
+import LoginForm from "./LoginForm";
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 
@@ -11,8 +11,8 @@ const MenuHeader = () => {
   const { t } = useTranslation();
   const [selectLoginForm, setLoginForm] = useState(false);
   const changeLanguage = (e) => {
-    i18n.changeLanguage (e.target.id)
-  }
+    i18n.changeLanguage(e.target.id);
+  };
 
   return (
     <Segment inverted>
@@ -23,18 +23,8 @@ const MenuHeader = () => {
             size="large"
             fluid="true"
           />
-          </Menu.Item>
-          <Button
-            icon labelPosition="left"
-            icon="user"
-            data-cy="login-btn"
-            as={ NavLink }
-            to={`/login/`}>
-            <Icon name="user"></Icon>
-            Login
-          </Button>
-        {selectLoginForm && <LoginForm />}
-          
+        </Menu.Item>
+
         <Menu.Item position="right">
           <Menu.Item>
             <Label
@@ -46,11 +36,26 @@ const MenuHeader = () => {
             />
             <MasterModal />
           </Menu.Item>
+          <Menu.Item>
+            <Button
+              icon
+              labelPosition="left"
+              icon="user"
+              data-cy="login-btn"
+              as={NavLink}
+              to={`/login/`}
+            >
+              <Icon name="user"></Icon>
+              Login
+            </Button>
+            {selectLoginForm && <LoginForm />}
+          </Menu.Item>
+
           <Menu.Item name="En">
             <Image
               data-cy="us-btn"
               id="en"
-              onClick={e => {
+              onClick={(e) => {
                 changeLanguage(e);
               }}
               src="./assets/united-kingdom.png"
@@ -63,7 +68,7 @@ const MenuHeader = () => {
             <Image
               data-cy="se-btn"
               id="sv"
-              onClick={e => {
+              onClick={(e) => {
                 changeLanguage(e);
               }}
               src="./assets/sweden.png"
